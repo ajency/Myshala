29e4b9344857da7dc0974adb773ad42dfb8fddf8
Jarryd <jarryd@ajency.in>
2013-02-25 13:25:40 +0530

    Buddypress Message Styles

--- wp-content/themes/myshala/members/single/messages/messages-loop.php
+++ wp-content/themes/myshala/members/single/messages/messages-loop.php
@@ -17,43 +17,44 @@
 	<?php do_action( 'bp_after_member_messages_pagination' ); ?>
 
 	<?php do_action( 'bp_before_member_messages_threads'   ); ?>
-
-	<table id="message-threads" class="messages-notices">
+	
+	<div id="message-threads" class="messages-notices">
 		<?php while ( bp_message_threads() ) : bp_message_thread(); ?>
-
-			<tr id="m-<?php bp_message_thread_id(); ?>" class="<?php bp_message_css_class(); ?><?php if ( bp_message_thread_has_unread() ) : ?> unread"<?php else: ?> read"<?php endif; ?>>
-				<td width="1%" class="thread-count">
-					<span class="unread-count"><?php bp_message_thread_unread_count(); ?></span>
-				</td>
-				<td width="1%" class="thread-avatar"><?php bp_message_thread_avatar(); ?></td>
-
+		<div id="m-<?php bp_message_thread_id(); ?>" class="m-container <?php if ( bp_message_thread_has_unread() ) : ?>unread"<?php else: ?> read"<?php endif; ?>>
+			<div class="m-left">
+			
+				<div class="thread-avatar">
+					<?php bp_message_thread_avatar(); ?>				
+				</div>
+				
+			</div><!-- /.m-left -->
+			<div class="m-right">
 				<?php if ( 'sentbox' != bp_current_action() ) : ?>
-					<td width="30%" class="thread-from">
-						<?php _e( 'From:', 'buddypress' ); ?> <?php bp_message_thread_from(); ?><br />
+					<div class="thread-from">
+						From: <?php bp_message_thread_from(); ?>
 						<span class="activity"><?php bp_message_thread_last_post_date(); ?></span>
-					</td>
+					</div>
 				<?php else: ?>
-					<td width="30%" class="thread-from">
-						<?php _e( 'To:', 'buddypress' ); ?> <?php bp_message_thread_to(); ?><br />
+					<div class="thread-from">
+						To: <?php bp_message_thread_to(); ?>
 						<span class="activity"><?php bp_message_thread_last_post_date(); ?></span>
-					</td>
+					</div>
 				<?php endif; ?>
-
-				<td width="50%" class="thread-info">
-					<p><a href="<?php bp_message_thread_view_link(); ?>" title="<?php _e( "View Message", "buddypress" ); ?>"><?php bp_message_thread_subject(); ?></a></p>
-					<p class="thread-excerpt"><?php bp_message_thread_excerpt(); ?></p>
-				</td>
-
-				<?php do_action( 'bp_messages_inbox_list_item' ); ?>
-
-				<td width="13%" class="thread-options">
-					<input type="checkbox" name="message_ids[]" value="<?php bp_message_thread_id(); ?>" />
-					<a class="button confirm" href="<?php bp_message_thread_delete_link(); ?>" title="<?php _e( "Delete Message", "buddypress" ); ?>"><?php _e( 'Delete', 'buddypress' ); ?></a> &nbsp;
-				</td>
-			</tr>
-
+				
+				<div class="thread-info">
+					<a href="<?php bp_message_thread_view_link(); ?>" title="<?php _e( "View Message", "buddypress" ); ?>"><?php bp_message_thread_subject(); ?></a>
+					<span><?php bp_message_thread_excerpt(); ?></span>
+				</div>
+				
+			</div><!-- /.m-right -->
+			<div class="clear"></div>
+			<div class="thread-options">
+				<input type="checkbox" name="message_ids[]" value="<?php bp_message_thread_id(); ?>" />
+				<a class="button confirm size-mini" title="Delete Message" href="<?php bp_message_thread_delete_link(); ?>" title="<?php _e( "Delete Message", "buddypress" ); ?>"><?php _e( 'Delete', 'buddypress' ); ?></a>
+			</div>
+		</div>
 		<?php endwhile; ?>
-	</table><!-- #message-threads -->
+	</div><!-- #message-threads -->
 
 	<div class="messages-options-nav">
 		<?php bp_messages_options(); ?>
