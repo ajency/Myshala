6b727c2aec04fe000f6c896ef1d68a042fe86d4a
Amit Kunjal <amitkunjal@ajency.in>
2013-04-03 10:42:06 +0530

    disallow non-admin users to access dashboard
    disallow non-admin users to access dashboard

--- wp-content/themes/myshala/functions.php
+++ wp-content/themes/myshala/functions.php
@@ -1929,3 +1929,11 @@ $current_user = wp_get_current_user();
 $bp_profile_link=bp_core_get_user_domain( $current_user->ID);
 bp_core_redirect($bp_profile_link);
 }
+
+/* FUNCTION TO DISALLOW NON ADMIN USERS TO ACCESS DASHBOARD */
+ add_action('admin_init', 'no_mo_dashboard');
+function no_mo_dashboard() {
+  if (!current_user_can('manage_options') && $_SERVER['DOING_AJAX'] != '/wp-admin/admin-ajax.php') {
+  wp_redirect(home_url()); exit;
+  }
+}
