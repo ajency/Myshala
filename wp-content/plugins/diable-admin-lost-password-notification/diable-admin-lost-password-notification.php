50f3f1ed470fb6156e5369dc88197806bea6eec7
Diana <diana@ajency.in>
2013-04-09 16:32:16 +0530

    diable admin chnge pass not

---
+++ wp-content/plugins/diable-admin-lost-password-notification/diable-admin-lost-password-notification.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Diable Admin Lost Password Notification
+Plugin URI: 
+Description: Diable Admin Lost Password Notification
+Version: 1.1.1
+Author: D
+Author URI:
+License: FR1
+*/
+ if ( !function_exists( 'wp_password_change_notification' ) ) {
+    function wp_password_change_notification() {}
+}
